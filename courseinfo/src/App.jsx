6c8d5152deddfd7ccbb8c446e16d2e19c59b7d25
Aditya dalvi <adityadalvi8910@gmail.com>
2025-01-22 17:04:24 +0530

    first commit

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,70 @@
+const Header = (props) => {
+  // console.log(props)
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  // console.log("Parts ==>",props)
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  // console.log("Content==>",props)
+  return (
+    <>
+      <Part part={props.part[0].name} exercises={props.part[0].exercises} />
+      <Part part={props.part[1].name} exercises={props.part[1].exercises} />
+      <Part part={props.part[2].name} exercises={props.part[2].exercises} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  const total = props.total
+  return <p>Number of exercises {total[0].exercises + total[1].exercises + total[2].exercises}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total total={course.parts}/> 
+    </>
+  );
+};
+
+
+// const arto = {
+//   name: 'Arto Hellas',
+//   greet: function() {
+//     console.log('hello, my name is ' + this.name)
+//   },
+// }
+
+// setTimeout(arto.greet(), 1000)
+// setTimeout(arto.greet.bind(arto), 1000)
+
+export default App;
